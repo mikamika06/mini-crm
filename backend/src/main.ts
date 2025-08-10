a27620110bea4edb703c1ed79e097f8e2cda7c1d
mikaMika <mikyseasbk@gmail.com>
2025-08-10 22:40:45 +0300

    Fix port configuration for Azure App Service

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  await app.listen(3333);
+  await app.listen(process.env.PORT || 3333);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
