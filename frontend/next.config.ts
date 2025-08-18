810135b01dd809edbf117444ac7fe8cb5e0bedad
mikamika06 <mikyseasbk@gmail.com>
2025-08-19 01:15:36 +0300

    try10

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,10 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.pravatar.cc'],
+    unoptimized: true, 
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3333',
   },
+  trailingSlash: true,
+  distDir: 'out',
+  output: 'export', 
 };
 
 module.exports = nextConfig;
